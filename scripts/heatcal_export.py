a5720e88c7a7449543772c0d3bcbda825d22369f
ninjawil <will.misc.acc@gmail.com>
2017-09-29 17:44:35 +0100

    Added read data from all years, sort it and write to file

--- scripts/heatcal_export.py
+++ scripts/heatcal_export.py
@@ -9,7 +9,7 @@ import sys
 import datetime
 import time
 import collections
-from xml.etree import cElementTree as ElementTree
+from xml.etree import cElementTree as ET
 
 # Third party modules
 
@@ -54,38 +54,91 @@ def main():
         sys.exit()  
 
 
-    #-------------------------------------------------------------------
-    # Get data from yearly files
-    #-------------------------------------------------------------------
     try:
-        tree = ElementTree.parse('{folder}/data/{xml_file}'.format(
-                                                    folder= folder_loc,
-                                                    xml_file= 'wd_all_2017.xml'))
-        root = tree.getroot()
-        xmldict = xml_tools.XmlDictConfig(root)
+        #-------------------------------------------------------------------
+        # Get data from yearly files
+        #-------------------------------------------------------------------      
+        sensors = []
+        xml_data = {}
 
-        values = {}
-        for child in root[1]:
-            for grandchild in child:
-                if grandchild.tag == 't':
-                    key = int(grandchild.text) / 1000
-                    values[key] = []
-                elif grandchild.tag == 'v':
-                    values[key].append(grandchild.text)
+        years = [str(year) for year in range(2016, datetime.datetime.now().year+1)]
+
+        for year_ref in years:
+
+            xml_file = 'wd_all_{d}.xml'.format(d= year_ref)
+
+            logger.info('Getting data from {f}'.format(f= xml_file)) 
+            tree = ET.parse('{folder}/data/{xml_file}'.format(
+                                                        folder= folder_loc,
+                                                        xml_file= xml_file))
+            logger.info('Sorting data')
+            
+            root = tree.getroot()
+            xmldict = xml_tools.XmlDictConfig(root)
+
+            for sensor in root.iter('entry'):
+                if sensor.text not in sensors:
+                    sensors.append(sensor.text)
+                    xml_data[sensor.text] = {}
+
+                if year_ref not in xml_data[sensor.text].keys():   
+                    xml_data[sensor.text][year_ref] = {}
 
 
+            # Sort data under sensor and then time
+            for child in root[1]:
+                for grandchild in child:
+                    if grandchild.tag == 't':
+                        time = str(int(grandchild.text) / 1000)
+                        i = 0
+                    elif grandchild.tag == 'v':
+                        xml_data[sensors[i]][year_ref][time] = grandchild.text
+                        i += 1
 
-        print values[1465344]
+
+        #-------------------------------------------------------------------
+        # Remove min and max data sets
+        #-------------------------------------------------------------------
+        for sensor in xml_data.keys():
+            if '_min' in sensor or '_max' in sensor:
+                del xml_data[sensor]
 
 
+        #-------------------------------------------------------------------
+        # Write data to xml file
+        #-------------------------------------------------------------------
 
-        print root[1][0][0].tag
-        print root[1][0][0].attrib
+        output_file = 'wd_all_years.xml'
+        logger.info('Writting data to {f}'.format(f=output_file))
+        # create new xml
+        root = ET.Element('root')
+        tree = ET.ElementTree(root)
+        data = ET.SubElement(root, 'data')
+
+        # populate xml
+        for op_xml in xml_data.keys():
+
+            # Create a subelement for each sensor 
+            sensor = ET.SubElement(data, 'sensor', name= op_xml) 
+
+            for year_ref in years:
+                year = ET.SubElement(sensor, 'year', year= year_ref)
+
+                # Populate time and value in each sensor          
+                for time, value in xml_data[op_xml][year_ref].iteritems():
+                    ET.SubElement(year, 'entry', time=time).text = value
+        
+
+        tree.write('{folder}/data/{xml_file}'.format(folder= folder_loc,
+                                                    xml_file= output_file))
 
 
     except Exception, e:
         logger.error('ERROR: ({error_v}).'.format(error_v=e), exc_info=True)
         sys.exit()
+
+    finally:
+        logger.info('--- Script {script} Finished ---'.format(script= script_name)) 
 
  
 
